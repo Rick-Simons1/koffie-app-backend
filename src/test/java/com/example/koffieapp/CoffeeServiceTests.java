d13f7168f086c5a0929883f5e4a74c2a4c1d79ce
Simons,Rick R.C.J <rick.simons@student.fontys.nl>
2020-06-02 15:26:50 +0200

    added classes annotation to test class to fix the error

--- src/test/java/com/example/koffieapp/CoffeeServiceTests.java
+++ src/test/java/com/example/koffieapp/CoffeeServiceTests.java
@@ -1,10 +1,11 @@
 package com.example.koffieapp;
 
+import koffieApp.KoffieAppApplication;
 import koffieApp.service.CoffeeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = CoffeeService.class)
+@SpringBootTest(classes = KoffieAppApplication.class)
 class KoffieAppApplicationTests {
 
 	@Test
